6f48cf42c7085c9e58a66e66f652bc7c7734dbd3
Wafia Durrani <wafiadr2@gmail.com>
2025-01-28 01:43:29 +0000

    Add files

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_6IM0EvHYtclF@ep-mute-cherry-a2h7f743.eu-central-1.aws.neon.tech/flame_lure_bulge_759277")
+
+os.environ.setdefault("SECRET_KEY", "###2525ABCD")
