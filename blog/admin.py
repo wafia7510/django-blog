afd221cbc22a9fdbc5335a9a4ab50a1f7e4d6a50
Wafia Durrani <wafiadr2@gmail.com>
2025-01-30 00:09:15 +0000

    new models have added

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Post
+from .models import Event
+from .models import Ticket
 admin.site.register(Post)
+admin.site.register(Event)
+admin.site.register(Ticket)
 # Register your models here.
