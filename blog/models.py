7dd2df608e1c84779ae92de1dbebe561f131c23f
wafia7510 <wafiadr2@gmail.com>
2025-01-31 16:29:21 +0000

    Add views and templates in blog app

--- blog/models.py
+++ blog/models.py
@@ -8,18 +8,29 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True);
-    slug = models.SlugField(max_length=200, unique=True);
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey( User , on_delete=models.CASCADE, related_name="blog_posts")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
+
     post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name="comments")
     author=models.ForeignKey(User,on_delete=models.CASCADE,related_name="commenter")
     body=models.TextField()
     approved=models.BooleanField(default=False)
     created_on=models.DateTimeField(auto_now_add=True)
+    challenge = models.SlugField(default='a_slug')
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
+from django.views import generic
+from .models import Post
+
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
